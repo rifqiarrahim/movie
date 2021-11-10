d26d232ba748382b1b2c919939e56a792ea8dd3b
rifqiarrahim <mrifqian@gmail.com>
2021-11-10 19:35:51 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    KEY: 'e7bf0117cb8e787a279e90ee0dfc22b6',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+    CACHE_NAME: new Date().toISOString(),
+  };
+   
+  export default CONFIG;
---
+++ src/scripts/sw.js
@@ -0,0 +1,19 @@
+import 'regenerator-runtime';
+import CacheHelper from './utils/cache-helper.js';
+const { assets } = global.serviceWorkerOption;
+self.addEventListener('install', (event) => {
+    event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
+   
+    // TODO: Caching App Shell Resource
+  });
+   
+  self.addEventListener('activate', (event) => {
+    event.waitUntil(CacheHelper.deleteOldCache());
+   
+    // TODO: Delete old caches
+  });
+   
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(CacheHelper.revalidateCache(event.request));
+    // TODO: Add/get fetch request to/from caches
+  });
